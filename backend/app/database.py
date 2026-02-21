7a4a21ec4be1e458e022f021420731ed4b05eff7
Bhanu Pratap Singh <119033807+Anuruddhpratap56@users.noreply.github.com>
2026-02-21 18:04:17 +0530

    Update database.py

--- backend/app/database.py
+++ backend/app/database.py
@@ -3,6 +3,11 @@ from sqlalchemy.orm import declarative_base
 
 from .config import settings
 
-engine = create_async_engine(settings.async_database_url, pool_pre_ping=True)
+# engine = create_async_engine(settings.async_database_url, pool_pre_ping=True)
+engine = create_async_engine(
+    settings.async_database_url,
+    pool_pre_ping=True,
+    connect_args={"statement_cache_size": 0}
+)
 SessionLocal = async_sessionmaker(bind=engine, autocommit=False, autoflush=False, expire_on_commit=False, class_=AsyncSession)
 Base = declarative_base()
